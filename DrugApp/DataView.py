36cf25d7aa2ff37f946fae97b1dad7448f95c805
StephenZou <zoucytt@163.com>
2021-07-06 18:05:27 +0800

    update0706

--- DrugApp/DataView.py
+++ DrugApp/DataView.py
@@ -2,7 +2,7 @@ from flask import (Blueprint, render_template)
 
 dv = Blueprint('data', __name__, url_prefix='/data')
 
-
+# https://www.cnblogs.com/daniel-niu/p/10344828.html
 @dv.route('/view', methods=('GET','POST'))
 def view_data():
     return render_template('common/base.html')
