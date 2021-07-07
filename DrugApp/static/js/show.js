62a232a909a3450cdba8aeaf343c0a53f4c0bb43
StephenZou <zoucytt@163.com>
2021-07-07 18:46:24 +0800

    update0707

---
+++ DrugApp/static/js/show.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+    $(".nav-link").click(function () {
+        $(this).siblings(".nav-treeview").slideToggle(300);
+        if($(this).parent().hasClass("menu-open")) {
+            $(this).parent().removeClass("menu-open");
+            $(this).removeClass("active");
+        } else {
+            $(this).parent().addClass("menu-open");
+            $(this).addClass("active");
+        }
+    });
+    setInterval(function () {
+        var wd = $(window).width();
+        var side = $(".main-sidebar");
+        if(wd < 991){
+            // side.css('visibility', 'hidden');
+            side.hide(200);
+        } else {
+            // side.css('visibility', 'visible');
+            side.show(200);
+        }
+
+    }, 100);
+    $(".nav-link-header").click(function () {
+        var side = $(".main-sidebar");
+        if (side.isVi)
+    });
+});
