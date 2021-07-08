42cc5bbeec0fbb6879a683abaabbd0fb26501259
StephenZou <zoucytt@163.com>
2021-07-08 17:44:25 +0800

    update0708

--- DrugApp/DataView.py
+++ DrugApp/DataView.py
@@ -1,4 +1,5 @@
 from flask import (Blueprint, render_template)
+from DrugApp.model import ViewModel
 
 dv = Blueprint('data', __name__, url_prefix='/data')
 
@@ -7,4 +8,8 @@ dv = Blueprint('data', __name__, url_prefix='/data')
 def view_data():
     diseases = ['disease1', 'disease2', 'disease3', 'disease4', 'disease5', 'disease6', 'disease7', 'disease8']
     drugs = ['drug1', 'drug2', 'drug3', 'drug4', 'drug5', 'drug6', 'drug7', 'drug8', 'drug9', 'drug10', 'drug11']
-    return render_template('home.html', diseases=diseases, drugs=drugs)
+    indications = []
+    compounds = []
+    targets = []
+    vm = ViewModel.ViewModel(diseases, drugs, indications, compounds, targets)
+    return render_template('home.html', vm=vm)
